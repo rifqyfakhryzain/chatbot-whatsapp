4a4b6492d06c09dc35dfad2bff099c2589e346ae
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2026-09-17 20:00:42 +0700

    feat: add prisma client configuration

---
+++ src/config/prisma.js
@@ -0,0 +1,17 @@
+require("dotenv/config");
+
+const { PrismaPg } = require("@prisma/adapter-pg");
+const { PrismaClient } = require("../generated/prisma");
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const adapter = new PrismaPg(pool);
+
+const prisma = new PrismaClient({
+  adapter,
+});
+
+module.exports = prisma;
