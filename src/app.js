8bfb84a9141f40343f6f4aea9678679ab42d9027
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2026-09-18 19:29:34 +0700

    feat: integrate schedule routes and error handling

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
 const express = require("express");
+const errorMiddleware = require("./middlewares/error.middleware");
+const scheduleRoutes = require("./routes/schedule.routes");
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api/schedules", scheduleRoutes);
+
+app.use(errorMiddleware);
 
 module.exports = app;
