35439e0e0dc6626d8bc856414996c57ae13277c5
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2026-09-19 13:40:49 +0700

    feat: initialize WhatsApp client with server

--- src/server.js
+++ src/server.js
@@ -1,19 +1,14 @@
 const app = require("./app");
 const createWhatsAppClient = require("./integrations/whatsapp/whatsapp.client");
+const { logServer } = require("./utils/logger");
 
 const PORT = 3000;
 
-async function startServer() {
-  try {
-    await createWhatsAppClient();
+app.listen(PORT, async () => {
+  const message = `ChatBot WhatsApp server is running on port ${PORT}`;
 
-    app.listen(PORT, () => {
-      console.log(`ChatBot WhatsApp server is running on port ${PORT}`);
-    });
-  } catch (error) {
-    console.error("Failed to start ChatBot WhatsApp:", error);
-    process.exit(1);
-  }
-}
+  console.log(message);
+  logServer(message);
 
-startServer();
+  await createWhatsAppClient();
+});
